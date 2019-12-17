4912aedf95482369fe72b59ad9369249ff46b4b5
sbrozyna11 <sbrozyna11@gmail.com>
2019-12-17 23:04:36 +0100

    constructor+destructor
    Signed-off-by: sbrozyna11 <sbrozyna11@gmail.com>

--- include/package.hpp
+++ include/package.hpp
@@ -7,8 +7,17 @@
 
 class Package {
 public:
-    Package(ElementID id) : id_(id) {}
-    Package() : id_(Package::number_){Package::number_ += 1;}
+    ///Package(ElementID id) : id_(id) {}
+
+    Package() : id_(Package::number_){
+        if(freed_IDs.empty()){
+            assigned_IDs.push_back(number_);
+            Package::number_ += 1;
+        }else{
+            assigned_IDs.push_back(freed_IDs.front());
+        }
+    }
+
     ~Package(){
         assigned_IDs.remove_if(Package::number_);
         freed_IDs.push_back(Package::number_);
@@ -20,6 +29,5 @@ private:
     static int number_;
     static std::list<int> assigned_IDs;
     static std::list<int> freed_IDs;
-    static std::set<int> set_;
 };
 #endif //C___SEMESTR_III_PACKAGE_HPP
--- src/package.cpp
+++ src/package.cpp
@@ -1,4 +1,3 @@
 #include "package.hpp"
 
 int Package::number_ = 0;
-std::set<int> Package::set_;
