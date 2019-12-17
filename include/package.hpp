6a23940a1dce8d6317835718b625cc4f052af0dd
sbrozyna11 <sbrozyna11@gmail.com>
2019-12-17 23:13:43 +0100

    Constructor (id)
    Signed-off-by: sbrozyna11 <sbrozyna11@gmail.com>

--- include/package.hpp
+++ include/package.hpp
@@ -4,30 +4,39 @@
 #include "types.hpp"
 #include <set>
 #include <list>
+#include <algorithm>
 
 class Package {
 public:
-    ///Package(ElementID id) : id_(id) {}
+    Package(ElementID id) : id_(id) {
+        bool already_assigned = (std::find(assigned_IDs.begin(), assigned_IDs.end(), id) != assigned_IDs.end());
+        if (already_assigned)
+            throw std::invalid_argument("Given ID is already in use!");
+        assigned_IDs.push_back(id);
+    }
 
-    Package() : id_(Package::number_){
-        if(freed_IDs.empty()){
+    Package() : id_(Package::number_) {
+        if (freed_IDs.empty()) {
             assigned_IDs.push_back(number_);
             Package::number_ += 1;
-        }else{
+        } else {
             assigned_IDs.push_back(freed_IDs.front());
         }
     }
 
-    ~Package(){
+    ~Package() {
         assigned_IDs.remove_if(Package::number_);
         freed_IDs.push_back(Package::number_);
-        freed_IDs.sort();
-    ;}
-    ElementID getID() const {return id_;}
+        freed_IDs.sort();;
+    }
+
+    ElementID getID() const { return id_; }
+
 private:
     ElementID id_ = 0;
     static int number_;
     static std::list<int> assigned_IDs;
     static std::list<int> freed_IDs;
 };
+
 #endif //C___SEMESTR_III_PACKAGE_HPP
