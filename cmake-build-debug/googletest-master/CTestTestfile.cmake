418a907f2ce15d90c649225c192cb5ee20c58500
sbrozyna11 <sbrozyna11@gmail.com>
2019-12-18 09:34:05 +0100

    Testing
    Signed-off-by: sbrozyna11 <sbrozyna11@gmail.com>

--- cmake-build-debug/googletest-master/CTestTestfile.cmake
+++ cmake-build-debug/googletest-master/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Informatyka/net_sim/googletest-master
-# Build directory: C:/Informatyka/net_sim/cmake-build-debug/googletest-master
+# Source directory: E:/Projects/C++/net_sim/googletest-master
+# Build directory: E:/Projects/C++/net_sim/cmake-build-debug/googletest-master
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
+++ cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Informatyka/net_sim/googletest-master/googlemock
-# Build directory: C:/Informatyka/net_sim/cmake-build-debug/googletest-master/googlemock
+# Source directory: E:/Projects/C++/net_sim/googletest-master/googlemock
+# Build directory: E:/Projects/C++/net_sim/cmake-build-debug/googletest-master/googlemock
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
