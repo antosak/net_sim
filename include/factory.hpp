c436cfecf76936331c351ea648e1a1880e39d1da
Michał Antos <antosak47@gmail.com>
2020-01-12 23:42:39 +0100

    fdsv

--- include/factory.hpp
+++ include/factory.hpp
@@ -1,108 +1,108 @@
-//// 35: Michał Antos (302815), Szymon Brożyna (309040)
-//
-//#ifndef NET_SIM_FACTORY_HPP
-//#define NET_SIM_FACTORY_HPP
-//
-//#include "nodes.hpp"
-//#include <algorithm>
-//
-//enum class NodeColor { UNVISITED, VISITED, VERIFIED };
-//
-//template <typename Node>
-//class NodeCollection{
-//public:
-//    using container_t = typename std::list<Node>;
-//    using iterator = typename container_t::iterator;
-//    using const_iterator = typename container_t::const_iterator;
-//
-//    void add(Node& node) {container.push_back(std::move(node));} // na emplace_back krzyczy standardowo
-//    void remove_by_id(ElementID id_);
-//
-//    NodeCollection<Node>::iterator find_by_id(ElementID id_) {return std::find_if(container.begin(),
-//            container.end(),[id_] (const Node& node) {return node.get_id() == id_;});}
-//
-//    NodeCollection<Node>::const_iterator find_by_id(ElementID id_) const {return std::find_if(container.cbegin(),
-//            container.cend(), [id_] (const Node& node) {return node.get_id() == id_;});}
-//
-//    const_iterator cbegin() const { return container.cbegin(); }
-//    const_iterator cend() const { return container.cend(); }
-//    iterator begin() { return container.begin(); }
-//    const_iterator begin() const { return container.cbegin(); }
-//    iterator end() { return container.end(); }
-//    const_iterator end() const { return container.cend(); }
-//
-//private:
-//    container_t container;
-//};
-//
-//template<typename Node>
-//void NodeCollection<Node>::remove_by_id(ElementID id_) {
-//    NodeCollection<Node>::iterator node_to_erase = find_by_id(id_);
-//    if (node_to_erase != container.end()) {
-//        container.erase(node_to_erase);
-//    }
-//}
-//
-//class Factory{
-//public:
-//    void add_ramp(Ramp&& ramp) {ramps.add(ramp);}
-//    void remove_ramp(ElementID id) {ramps.remove_by_id(id);}
-//    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return ramps.find_by_id(id);}
-//    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const {return ramps.find_by_id(id);}
-//    NodeCollection<Ramp>::const_iterator ramp_cbegin() const {return ramps.cbegin();}
-//    NodeCollection<Ramp>::const_iterator ramp_cend() const {return ramps.cend();}
-//
-//    void add_worker(Worker&& worker) {workers.add(worker);}
-//    void remove_worker(ElementID id) {workers.remove_by_id(id);}
-//    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return workers.find_by_id(id);}
-//    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return workers.find_by_id(id);}
-//    NodeCollection<Worker>::const_iterator worker_cbegin() const {return workers.cbegin();}
-//    NodeCollection<Worker>::const_iterator worker_cend() const {return workers.cend();}
-//
-//    void add_storehouse(Storehouse&& storehouse) {storehouses.add(storehouse);}
-//    void remove_storehouse(ElementID id) {storehouses.remove_by_id(id);}
-//    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return storehouses.find_by_id(id);}
-//    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return storehouses.find_by_id(id);}
-//    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return storehouses.cbegin();}
-//    NodeCollection<Storehouse>::const_iterator storehouse_cend() const {return storehouses.cend();}
-//
-//    void do_deliveries(Time t);
-//    void do_package_passing();
-//    void do_work(Time t);
-//
-//
-//    bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
-//    bool is_consistent();
-//
-//
-//private:
-//    NodeCollection<Ramp> ramps;
-//    NodeCollection<Worker> workers;
-//    NodeCollection<Storehouse> storehouses;
-//    template <typename Node>
-//    void remove_receiver(NodeCollection<Node>& collection, ElementID id);
-//};
-//
-//template<typename Node>
-//void Factory::remove_receiver(NodeCollection<Node> &collection, ElementID id) {
-//    for (auto &worker_ : workers) {
-//        try {
-//            worker_.receiver_preferences_.remove_receiver(NodeCollection<Node>::find_by_id(id));
-//        } catch (const std::logic_error &e) {
-//            e.what();
-//            continue;
-//        }
-//    }
-//    for (auto &ramp_ : ramps) {
-//        try {
-//            ramp_.receiver_preferences_.remove_receiver(NodeCollection<Node>::find_by_id(id));
-//        } catch (const std::logic_error &e) {
-//            e.what();
-//            continue;
-//        }
-//    }
-//    collection.remove_by_id(id);
-//}
-//
-//
-//#endif //NET_SIM_FACTORY_HPP
+// 35: Michał Antos (302815), Szymon Brożyna (309040)
+
+#ifndef NET_SIM_FACTORY_HPP
+#define NET_SIM_FACTORY_HPP
+
+#include "nodes.hpp"
+#include <algorithm>
+
+enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+
+template <typename Node>
+class NodeCollection{
+public:
+    using container_t = typename std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
+
+    void add(Node& node) {this -> container.push_back(std::move(node));} // na emplace_back krzyczy standardowo
+    void remove_by_id(ElementID id_);
+
+    NodeCollection<Node>::iterator find_by_id(ElementID id_) {return std::find_if(container.begin(),
+            container.end(),[id_] (const Node& node) {return node.get_id() == id_;});}
+
+    NodeCollection<Node>::const_iterator find_by_id(ElementID id_) const {return std::find_if(container.cbegin(),
+            container.cend(), [id_] (const Node& node) {return node.get_id() == id_;});}
+
+    const_iterator cbegin() const { return container.cbegin(); }
+    const_iterator cend() const { return container.cend(); }
+    iterator begin() { return container.begin(); }
+    const_iterator begin() const { return container.cbegin(); }
+    iterator end() { return container.end(); }
+    const_iterator end() const { return container.cend(); }
+
+private:
+    container_t container;
+};
+
+template<typename Node>
+void NodeCollection<Node>::remove_by_id(ElementID id_) {
+    NodeCollection<Node>::iterator node_to_erase = find_by_id(id_);
+    if (node_to_erase != container.end()) {
+        container.erase(node_to_erase);
+    }
+}
+
+class Factory{
+public:
+    void add_ramp(Ramp&& ramp) {ramps.add(ramp);}
+    void remove_ramp(ElementID id) {ramps.remove_by_id(id);}
+    NodeCollection<Ramp>::iterator find_ramp_by_id(ElementID id) {return ramps.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(ElementID id) const {return ramps.find_by_id(id);}
+    NodeCollection<Ramp>::const_iterator ramp_cbegin() const {return ramps.cbegin();}
+    NodeCollection<Ramp>::const_iterator ramp_cend() const {return ramps.cend();}
+
+    void add_worker(Worker&& worker) {workers.add(worker);}
+    void remove_worker(ElementID id) {workers.remove_by_id(id);}
+    NodeCollection<Worker>::iterator find_worker_by_id(ElementID id) {return workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator find_worker_by_id(ElementID id) const {return workers.find_by_id(id);}
+    NodeCollection<Worker>::const_iterator worker_cbegin() const {return workers.cbegin();}
+    NodeCollection<Worker>::const_iterator worker_cend() const {return workers.cend();}
+
+    void add_storehouse(Storehouse&& storehouse) {storehouses.add(storehouse);}
+    void remove_storehouse(ElementID id) {storehouses.remove_by_id(id);}
+    NodeCollection<Storehouse>::iterator find_storehouse_by_id(ElementID id) {return storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator find_storehouse_by_id(ElementID id) const {return storehouses.find_by_id(id);}
+    NodeCollection<Storehouse>::const_iterator storehouse_cbegin() const {return storehouses.cbegin();}
+    NodeCollection<Storehouse>::const_iterator storehouse_cend() const {return storehouses.cend();}
+
+    void do_deliveries(Time t);
+    void do_package_passing();
+    void do_work(Time t);
+
+
+    bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
+    bool is_consistent();
+
+
+private:
+    NodeCollection<Ramp> ramps;
+    NodeCollection<Worker> workers;
+    NodeCollection<Storehouse> storehouses;
+    template <typename Node>
+    void remove_receiver(NodeCollection<Node>& collection, ElementID id);
+};
+
+template<typename Node>
+void Factory::remove_receiver(NodeCollection<Node> &collection, ElementID id) {
+    for (auto &worker_ : workers) {
+        try {
+            worker_.receiver_preferences_.remove_receiver(NodeCollection<Node>::find_by_id(id));
+        } catch (const std::logic_error &e) {
+            e.what();
+            continue;
+        }
+    }
+    for (auto &ramp_ : ramps) {
+        try {
+            ramp_.receiver_preferences_.remove_receiver(NodeCollection<Node>::find_by_id(id));
+        } catch (const std::logic_error &e) {
+            e.what();
+            continue;
+        }
+    }
+    collection.remove_by_id(id);
+}
+
+
+#endif //NET_SIM_FACTORY_HPP
--- src/factory.cpp
+++ src/factory.cpp
@@ -1,84 +1,84 @@
-//// 35: Michał Antos (302815), Szymon Brożyna (309040)
-//
-//#include "factory.hpp"
-//
-//bool Factory::is_consistent() {
-//    std::map<const PackageSender *, NodeColor> node_colors;
-//    for (auto &worker_ : workers) {
-//        node_colors[&worker_] = NodeColor::UNVISITED;
-//    }
-//    for (auto &ramp_: ramps) {
-//        node_colors[&ramp_] = NodeColor::UNVISITED;
-//    }
-//    try {
-//        for (auto &ramp_: ramps) {
-//            has_reachable_storehouse(&ramp_, node_colors);
-//        }
-//    } catch (const std::logic_error &e) {
-//        e.what();
-//        return false;
-//    }
-//    return true;
-//}
-//
-//void Factory::do_work(Time t) {
-//    for (auto &worker_ : workers) {
-//        worker_.do_work(t);
-//    }
-//}
-//
-//void Factory::do_package_passing() {
-//    for (auto &worker_ : workers) {
-//        if (worker_.buffer) {
-//            worker_.send_package();
-//        }
-//    }
-//    for (auto &ramp_: ramps) {
-//        if (ramp_.buffer) {
-//            ramp_.send_package();
-//        }
-//    }
-//}
-//
-//void Factory::do_deliveries(Time t) {
-//    for (auto &ramp_ : ramps) {
-//        ramp_.deliver_goods(t);
-//    }
-//}
-//
-//bool Factory::has_reachable_storehouse(const PackageSender *sender,
-//                                       std::map<const PackageSender *, NodeColor> &node_colors) {
-//
-//    if (node_colors[sender] == NodeColor::VERIFIED)
-//        return true;
-//    else
-//        node_colors[sender] = NodeColor::VISITED;
-//
-//    if (sender->receiver_preferences_.get_preferences().empty()) {
-//        throw std::logic_error("No desired receiver.");
-//    }
-//
-//    bool does_sender_have_receiver = false;
-//
-//    for (auto receiver_ : sender->receiver_preferences_.get_preferences()) {
-//        if (receiver_.first->get_receiver_type() == ReceiverType::STOREHOUSE) {
-//            does_sender_have_receiver = true;
-//        } else {
-//            IPackageReceiver *receiver_ptr = receiver_.first;
-//            auto worker_ptr = dynamic_cast<Worker *>(receiver_ptr);
-//            auto sendrecv_ptr = dynamic_cast<PackageSender *>(worker_ptr);
-//            if (worker_ptr == sender) {
-//                continue;
-//            } else if (node_colors[sendrecv_ptr] == NodeColor::UNVISITED) {
-//                does_sender_have_receiver = has_reachable_storehouse(sendrecv_ptr, node_colors);
-//            }
-//        }
-//        node_colors[sender] = NodeColor::VERIFIED;
-//    }
-//    if(does_sender_have_receiver){
-//        return true;
-//    }
-//    return false;
-//}
-//
-//
+// 35: Michał Antos (302815), Szymon Brożyna (309040)
+
+#include "factory.hpp"
+
+bool Factory::is_consistent() {
+    std::map<const PackageSender *, NodeColor> node_colors;
+    for (auto &worker_ : workers) {
+        node_colors[&worker_] = NodeColor::UNVISITED;
+    }
+    for (auto &ramp_: ramps) {
+        node_colors[&ramp_] = NodeColor::UNVISITED;
+    }
+    try {
+        for (auto &ramp_: ramps) {
+            has_reachable_storehouse(&ramp_, node_colors);
+        }
+    } catch (const std::logic_error &e) {
+        e.what();
+        return false;
+    }
+    return true;
+}
+
+void Factory::do_work(Time t) {
+    for (auto &worker_ : workers) {
+        worker_.do_work(t);
+    }
+}
+
+void Factory::do_package_passing() {
+    for (auto &worker_ : workers) {
+        if (worker_.buffer) {
+            worker_.send_package();
+        }
+    }
+    for (auto &ramp_: ramps) {
+        if (ramp_.buffer) {
+            ramp_.send_package();
+        }
+    }
+}
+
+void Factory::do_deliveries(Time t) {
+    for (auto &ramp_ : ramps) {
+        ramp_.deliver_goods(t);
+    }
+}
+
+bool Factory::has_reachable_storehouse(const PackageSender *sender,
+                                       std::map<const PackageSender *, NodeColor> &node_colors) {
+
+    if (node_colors[sender] == NodeColor::VERIFIED)
+        return true;
+    else
+        node_colors[sender] = NodeColor::VISITED;
+
+    if (sender->receiver_preferences_.get_preferences().empty()) {
+        throw std::logic_error("No desired receiver.");
+    }
+
+    bool does_sender_have_receiver = false;
+
+    for (auto receiver_ : sender->receiver_preferences_.get_preferences()) {
+        if (receiver_.first->get_receiver_type() == ReceiverType::STOREHOUSE) {
+            does_sender_have_receiver = true;
+        } else {
+            IPackageReceiver *receiver_ptr = receiver_.first;
+            auto worker_ptr = dynamic_cast<Worker *>(receiver_ptr);
+            auto sendrecv_ptr = dynamic_cast<PackageSender *>(worker_ptr);
+            if (worker_ptr == sender) {
+                continue;
+            } else if (node_colors[sendrecv_ptr] == NodeColor::UNVISITED) {
+                does_sender_have_receiver = has_reachable_storehouse(sendrecv_ptr, node_colors);
+            }
+        }
+        node_colors[sender] = NodeColor::VERIFIED;
+    }
+    if(does_sender_have_receiver){
+        return true;
+    }
+    return false;
+}
+
+
