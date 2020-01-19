e6472feb9c18849b0590338a3d0a1adb393c6e4a
Michał Antos <antosak47@gmail.com>
2020-01-19 20:25:47 +0100

    interval class

--- include/simulation.hpp
+++ include/simulation.hpp
@@ -8,4 +8,12 @@
 
 void simulate(Factory &factory, TimeOffset d, std::function<void(Factory &, Time)>);
 
+class IntervalReportNotifier{
+public:
+    IntervalReportNotifier(TimeOffset to_) : to(to_) {}
+    bool should_generate_report(Time t) {return t - 1 % to == 0;};
+private:
+    TimeOffset to;
+};
+
 #endif //NET_SIM_SIMULATION_HPP
